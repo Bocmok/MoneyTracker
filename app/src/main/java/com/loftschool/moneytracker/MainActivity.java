76ccb7317cfcb227cee712870be73688d76109ce
Bocmok <m_mnishek@INBOX.RU>
2018-07-13 19:51:01 +0300

    exercise_04

--- app/src/main/java/com/loftschool/moneytracker/MainActivity.java
+++ app/src/main/java/com/loftschool/moneytracker/MainActivity.java
@@ -1,44 +1,28 @@
 package com.loftschool.moneytracker;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager =findViewById(R.id.viewPager);
+
+        tabLayout =findViewById(R.id.tabLayout);
+        tabLayout.setSelectedTabIndicatorColor(getResources().getColor(R.color.colorIndicator));
+        tabLayout.setTabTextColors(getResources().getColor(R.color.colorInactive),getResources().getColor(R.color.colorActive));
+
+        MainPagesAdapter adapter = new MainPagesAdapter(getSupportFragmentManager(),this);
+        viewPager.setAdapter(adapter);
+
+        tabLayout.setupWithViewPager(viewPager);
     }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.i( "MainActivity", "onStop");
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.i("MainActivity", "onStart");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.i("MainActivity","onPause");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.i("MainActivity","onResume");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.i("MainActivity", "onDestroy");
-    }
 }
---
+++ app/src/main/java/com/loftschool/moneytracker/MainPagesAdapter.java
@@ -0,0 +1,47 @@
+package com.loftschool.moneytracker;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+
+public class MainPagesAdapter extends FragmentPagerAdapter {
+
+    public static final int PAGE_INCOMES=0;
+    public static final int PAGE_EXPENSES=1;
+    public static final int PAGE_BALANCE=2;
+    private String[] titles;
+
+    public MainPagesAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        titles=context.getResources().getStringArray(R.array.tab_titles);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case PAGE_INCOMES:
+                return ItemsFragment.createItemsFragment(ItemsFragment.TYPE_INCOMES);
+            case PAGE_EXPENSES:
+                return ItemsFragment.createItemsFragment(ItemsFragment.TYPE_EXPENSES);
+            case PAGE_BALANCE:
+                return BalanceFragment.createBalanceFragment();
+            default:
+                return null;
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return titles[position];
+    }
+}
