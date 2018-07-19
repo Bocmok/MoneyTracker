511b4871083367e61a197492b46c2de31290269b
Bocmok <m_mnishek@INBOX.RU>
2018-07-19 21:57:39 +0300

    add TextWatcher

--- app/src/main/java/com/loftschool/moneytracker/AddItemActivity.java
+++ app/src/main/java/com/loftschool/moneytracker/AddItemActivity.java
@@ -2,9 +2,6 @@ package com.loftschool.moneytracker;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
-import android.text.TextUtils;
-import android.text.TextWatcher;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -12,7 +9,6 @@ public class AddItemActivity extends AppCompatActivity {
     private EditText name;
     private EditText price;
     private Button addBtn;
-    private int n;
 
 
     @Override
@@ -25,27 +21,35 @@ public class AddItemActivity extends AppCompatActivity {
         price=findViewById(R.id.price);
         addBtn=findViewById(R.id.add_btn);
 
+        EditText[] edList = {name, price};
+        CustomTextWatcher textWatcher = new CustomTextWatcher(edList, addBtn);
+        for (EditText editText : edList) editText.addTextChangedListener(textWatcher);
+
+    }
 
 
-        name.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+}
 
-            }
 
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                final String itemPrice=price.getText().toString();
-                if (!TextUtils.isEmpty(itemPrice)) n=1;
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-
-                if ((!TextUtils.isEmpty(editable))&(n==1)) addBtn.setEnabled(true);
-            }
-        });
+//        name.addTextChangedListener(new TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//                final String itemPrice=price.getText().toString();
+//                if (!TextUtils.isEmpty(itemPrice)) n=1;
+//
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable editable) {
+//
+//                if ((!TextUtils.isEmpty(editable))&(n==1)) addBtn.setEnabled(true);
+//            }
+//        });
 
 
 
@@ -57,6 +61,6 @@ public class AddItemActivity extends AppCompatActivity {
 //                String itemPrice=price.getText().toString();
 //            }
 //        });
-    }
 
-}
+
+
